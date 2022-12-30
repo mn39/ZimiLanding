2a815b5c6f001fa6ea39ad06463905dcc95c9c8a
mn <mn@mnui-MacBookPro.local>
2022-12-30 16:58:11 +0900

    robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,8 +57,13 @@ export default {
   buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
+  modules: ['@nuxtjs/robots', '@nuxtjs/sitemap'],
+  robots: [
+    {
+      UserAgent: '*',
+      Disallow: '',
+    },
+  ],
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
